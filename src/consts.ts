8b697a25e3ebb58a755e5fa61515dc79a4856dbe
Alex Helber <helber-alx@mail.ru>
2025-02-19 17:31:02 +0300

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
+  TITLE: "Blog",
+  DESCRIPTION: "My IT Blog",
+  EMAIL: "alexander@helber.ru",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
